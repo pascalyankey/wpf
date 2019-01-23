26681a847bc4cacc2a38d7dfb856fcb7888de787
Pascal Yankey <pascalyankey@gmail.com>
2019-01-23 15:59:23 +0100

    Add project files.

---
+++ ButtonGebruik/ButtonGebruikWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonGebruik
+{
+    /// <summary>
+    /// Interaction logic for ButtonGebruikWindow.xaml
+    /// </summary>
+    public partial class ButtonGebruikWindow : Window
+    {
+        public ButtonGebruikWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBold_Checked(object sender, RoutedEventArgs e)
+        {
+            LabelTekst.FontWeight = FontWeights.Bold;
+            CheckBoxBold.IsChecked = ButtonBold.IsChecked;
+        }
+
+        private void ButtonBold_Unchecked(object sender, RoutedEventArgs e)
+        {
+            LabelTekst.FontWeight = FontWeights.Normal;
+            CheckBoxBold.IsChecked = ButtonBold.IsChecked;
+        }
+
+        private void ButtonItalic_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton knop = (ToggleButton)sender;
+
+            if (knop.IsChecked == true)
+                LabelTekst.FontStyle = FontStyles.Italic;
+            else
+                LabelTekst.FontStyle = FontStyles.Normal;
+            CheckBoxItalic.IsChecked = knop.IsChecked;
+            ButtonItalic.IsChecked = knop.IsChecked;
+        }
+
+        private void RepeatButtonGroter_Click(object sender, RoutedEventArgs e)
+        {
+            if (LabelTekst.FontSize < 25)
+                LabelTekst.FontSize++;
+        }
+
+        private void RepeatButtonKleiner_Click(object sender, RoutedEventArgs e)
+        {
+            if (LabelTekst.FontSize > 1)
+                LabelTekst.FontSize--;
+        }
+
+        private void Kleur_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton knop = (RadioButton)sender;
+            LabelTekst.Foreground = (SolidColorBrush)new BrushConverter().ConvertFromString(knop.Content.ToString());
+        }
+
+        private void CheckBoxBold_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonBold.IsChecked = CheckBoxBold.IsChecked;
+        }
+
+        private void CheckBoxItalic_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton knop = (ToggleButton)sender;
+
+            if (knop.IsChecked == true)
+                LabelTekst.FontStyle = FontStyles.Italic;
+            else
+                LabelTekst.FontStyle = FontStyles.Normal;
+            CheckBoxItalic.IsChecked = knop.IsChecked;
+            ButtonItalic.IsChecked = knop.IsChecked;
+        }
+    }
+}
---
+++ VerkeerslichtOpdracht/Verkeerslicht.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace VerkeerslichtOpdracht
+{
+    /// <summary>
+    /// Interaction logic for Verkeerslicht.xaml
+    /// </summary>
+    public partial class Verkeerslicht : Window
+    {
+        public Verkeerslicht()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            OranjeLicht.Opacity = 0;
+            GroenLicht.Opacity = 1;
+            ButtonOpgelet.IsEnabled = true;
+            ButtonGo.IsEnabled = false;
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            OranjeLicht.Opacity = 0;
+            RoodLicht.Opacity = 1;
+            ButtonOpgelet.IsEnabled = true;
+            ButtonStop.IsEnabled = false;
+        }
+
+        private void ButtonOpgelet_Click(object sender, RoutedEventArgs e)
+        {
+            if (GroenLicht.Opacity == 1)
+            {
+                ButtonStop.IsEnabled = true;
+                GroenLicht.Opacity = 0;
+            }
+            else
+            {
+                ButtonGo.IsEnabled = true;
+                RoodLicht.Opacity = 0;
+            }
+            OranjeLicht.Opacity = 1;
+            ButtonOpgelet.IsEnabled = false;
+        }
+    }
+}
